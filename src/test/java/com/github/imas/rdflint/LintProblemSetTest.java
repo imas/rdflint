4b5e9a7fc671e8e7d86aec1ff1b6d9d25598b664
Petr Kremen <kremep1@fel.cvut.cz>
2020-03-20 10:19:00 +0100

    fixed long line

--- src/test/java/com/github/imas/rdflint/LintProblemSetTest.java
+++ src/test/java/com/github/imas/rdflint/LintProblemSetTest.java
@@ -3,25 +3,25 @@ package com.github.imas.rdflint;
 import com.github.imas.rdflint.validator.RdfValidator;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class LintProblemSetTest {
 
-  @Test public void minimalErrorLevelCorrectlyUsed() throws Exception {
+  @Test public void minimalErrorLevelCorrectlyUsed() {
     LintProblemSet problemSet = new LintProblemSet();
-    RdfValidator validator= null;
+    RdfValidator v= null;
     LintProblemLocation location = null;
     String key = null;
     String fileName = "";
-    problemSet.addProblem(fileName, new LintProblem(LintProblem.ErrorLevel.WARN, validator,location,key));
-    assertFalse( problemSet.hasProblemOfLevelOrWorse(LintProblem.ErrorLevel.ERROR));
-    assertTrue( problemSet.hasProblemOfLevelOrWorse(LintProblem.ErrorLevel.WARN));
+    problemSet.addProblem(fileName, new LintProblem(LintProblem.ErrorLevel.WARN, v,location,key));
+    assertFalse(problemSet.hasProblemOfLevelOrWorse(LintProblem.ErrorLevel.ERROR));
+    assertTrue(problemSet.hasProblemOfLevelOrWorse(LintProblem.ErrorLevel.WARN));
 
-    problemSet.addProblem(fileName, new LintProblem(LintProblem.ErrorLevel.INFO, validator,location,key));
-    assertFalse( problemSet.hasProblemOfLevelOrWorse(LintProblem.ErrorLevel.ERROR));
+    problemSet.addProblem(fileName, new LintProblem(LintProblem.ErrorLevel.INFO, v,location,key));
+    assertFalse(problemSet.hasProblemOfLevelOrWorse(LintProblem.ErrorLevel.ERROR));
 
-    problemSet.addProblem(fileName, new LintProblem(LintProblem.ErrorLevel.ERROR, validator,location,key));
-    assertTrue( problemSet.hasProblemOfLevelOrWorse(LintProblem.ErrorLevel.ERROR));
+    problemSet.addProblem(fileName, new LintProblem(LintProblem.ErrorLevel.ERROR, v,location,key));
+    assertTrue(problemSet.hasProblemOfLevelOrWorse(LintProblem.ErrorLevel.ERROR));
   }
 }
